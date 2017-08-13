ac7e19f1d756ba0139d3d87bd9273710d5d8f9f2
Yuchuan Gou <plain1994@gmail.com>
2017-08-13 22:38:06 +0800

    edit

--- DLRanking/settings.py
+++ DLRanking/settings.py
@@ -125,3 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
     ]
+
+STATICFILES_FINDERS = (
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
